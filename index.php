723ef17a7b3ed0dc962c1b4aa83b57892692702c
Asad Meer <meerasads636@gmail.com>
2024-05-06 23:36:01 +0500

    google analytic ID add

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 // +------------------------------------------------------------------------+
 // | Copyright (c) 2017 FUNBOOK. All rights reserved.
 // +------------------------------------------------------------------------+
-
 date_default_timezone_set('UTC');
 include_once("conf/site-config.php");
 include_once("conf/frameworkLoader.php");
@@ -23,4 +22,14 @@ if(!is_dir("errorLogs")) {
 }
 $oAzFramework->addRoutes(require 'AppRoutes.php');
 $oAzFramework->run();
+?>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LGQ7BSJHH0"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LGQ7BSJHH0');
+</script>
